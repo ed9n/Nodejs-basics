90dbc5496b5faa778055aba71f803499826956b5
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-01 00:38:12 +0300

    feat: add function read

--- nodejs-basics/src/fs/read.js
+++ nodejs-basics/src/fs/read.js
@@ -1,3 +1,10 @@
+import { readFile } from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    readFile('./files/fileToRead.txt', 'utf8', (err, content) => {
+        if (err) throw new Error('FS operation failed');
+        console.log(content);
+    });
 };
+
+read();
