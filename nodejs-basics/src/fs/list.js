4e193890228b94f03d84d817618cc9e86b9612fd
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-01 00:31:35 +0300

    feat: add function list

--- nodejs-basics/src/fs/list.js
+++ nodejs-basics/src/fs/list.js
@@ -1,3 +1,12 @@
+import { readdir } from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+    readdir('./files/', (err, files) => {
+        if (err) throw new Error('FS operation failed');
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
 };
+
+list();
