6365b6ed5383dc5eabecf1c92ca1b41a6371ffff
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-05-31 18:54:17 +0300

    feat: add function create

--- nodejs-basics/src/fs/create.js
+++ nodejs-basics/src/fs/create.js
@@ -1,3 +1,26 @@
+import { access, open, writeFile } from 'fs';
+
+const path = './files/fresh.txt';
+
 export const create = async () => {
-    // Write your code here 
+    access(path, (err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        } else {
+
+            open(path, 'w', (err) => {
+                if (err) {
+                    throw new Error(err);
+                }
+            });
+
+            writeFile(path, 'I am fresh and young', (err) => {
+                if (err) {
+                    throw new Error(err);
+                }
+            });
+        }
+    });
 };
+
+create();
