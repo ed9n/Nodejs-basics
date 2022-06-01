019910c25d0fea0c9b0119833bcfebed197c4a79
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-01 20:12:54 +0300

    feat: add function parseArgs

--- nodejs-basics/src/cli/args.js
+++ nodejs-basics/src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
+    process.argv.forEach((val, index) => {
+        if (index == 0) {
+            console.log(`prop Node is ${val}`);
+        } else if (index == 1) {
+            console.log(`prop JS is ${val}`);
+        } else {
+            console.log(`Your prop is ${val}`);
+        };
+    });
 };
+
+parseArgs();
