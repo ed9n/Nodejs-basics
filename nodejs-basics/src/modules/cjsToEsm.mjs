cd5c03e57a9ab21a3f9f809db1b1d14f9fff7fe0
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-02 00:31:51 +0300

    refactor: rewrited on equivalent in ECMAScript

---
+++ nodejs-basics/src/modules/cjsToEsm.mjs
@@ -0,0 +1,41 @@
+import { sep, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { release, version } from 'os';
+import { readFile } from 'fs';
+import { createServer as createServerHttp } from 'http';
+import { } from './files/c.js';
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = readFile('./files/a.json', 'utf8', (err, data) => {
+        if (err) throw new Error(err);
+        console.log(JSON.parse(data));
+    });
+} else {
+    unknownObject = readFile('./files/b.json', 'utf8', (err, data) => {
+        if (err) throw new Error(err);
+        console.log(JSON.parse(data));
+    });
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const createMyServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+export default {
+    unknownObject,
+    createMyServer,
+};
+
