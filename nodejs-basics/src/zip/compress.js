dbca9ab5c9ffe385d4b4aeb6a3159ab3e3f215c9
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-03 17:57:02 +0300

    feat: add function compress

--- nodejs-basics/src/zip/compress.js
+++ nodejs-basics/src/zip/compress.js
@@ -1,3 +1,11 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const input = createReadStream('./files/fileToCompress.txt');
+    const output = createWriteStream('archive.gz');
+    input.pipe(gzip).pipe(output);
 };
+
+compress();
