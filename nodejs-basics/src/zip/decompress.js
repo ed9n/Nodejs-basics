7f875fb79dfa939c292a6b98340dca2a5866fc86
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-03 18:01:37 +0300

    feat: add function decompress

--- nodejs-basics/src/zip/decompress.js
+++ nodejs-basics/src/zip/decompress.js
@@ -1,3 +1,11 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 export const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    const input = createReadStream('archive.gz');
+    const output = createWriteStream('fileToCompress.txt');
+    input.pipe(unzip).pipe(output);
 };
+
+decompress();
