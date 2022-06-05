c821d987cd26ad47b74a6d3248022fc03efb0e9a
jygava97@gmail.com <eduard.rzhautski@gmail.com>
2022-06-05 22:06:40 +0300

    feat: add function main.js and worker.js

--- nodejs-basics/src/wt/main.js
+++ nodejs-basics/src/wt/main.js
@@ -1,3 +1,26 @@
+import { Worker, workerData } from 'worker_threads';
+import { cpus } from 'os';
+
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpuCount = cpus().length;
+    let count = 10;
+    let arr = [];
+    for (let i = 0; i < cpuCount; i++) {
+        const Workers = new Promise((resolve, reject) => {
+            const worker = new Worker(`./worker.js`, { workerData: count++ })
+            worker.postMessage(workerData);
+            worker.on('message', resolve);
+            worker.on('error', reject);
+        })
+
+        try {
+            arr.push(await Workers);
+        } catch {
+
+        }
+    }
+    console.log(arr)
 };
+
+performCalculations();
--- nodejs-basics/src/wt/worker.js
+++ nodejs-basics/src/wt/worker.js
@@ -1,6 +1,23 @@
+import { workerData, parentPort } from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+
+    if (workerData == null || workerData == undefined || typeof workerData != 'number') {
+        const errData = {
+            status: 'error',
+            data: null
+        }
+        parentPort.postMessage(errData);
+    } else {
+        const data = {
+            status: 'resolved',
+            data: nthFibonacci(workerData)
+        }
+        parentPort.postMessage(data);
+    }
 };
+
+sendResult();
